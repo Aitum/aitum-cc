69525a4e3b5bf1b25f8e54892616c8b9a61c2ad6
David Marsh <david@aitum.tv>
2022-12-10 04:07:57 +0000

    Redo for registration

--- src/actions/DummyAction.ts
+++ src/actions/DummyAction.ts
@@ -1,5 +1,38 @@
-export default async function method (inputs: { [key: string]: number | string | boolean | string[] }) {
+import { BooleanInput, FloatInput, ICCActionInputs, ICustomCode, IntInput, StringInput } from 'aitum-cc-lib';
+import { AitumJS } from 'aitum.js';
+import { DeviceType } from 'aitum.js/src/enums/DeviceType';
 
+/*********** CONFIG ***********/
+// The custom code action name
+const name: string = 'Dummy Action';
+
+// The custom code inputs
+const inputs: ICCActionInputs = {
+  testStringInput: new StringInput('What is your name?', { required: false }),
+  testBooleanInput: new BooleanInput('Are you a fun person?', { required: false }),
+  testIntInput: new IntInput('How old are you?', { required: false }),
+  testFloatInput: new FloatInput('Volume level', { required: false })
+}
+
+// The code executed.
+async function method(inputs: { [key: string]: number | string | boolean | string[] }) {
+  console.log(inputs)
   console.log('in method')
 
+  const lib = AitumJS.get();
+
+  const aitumDevice = (await lib.getDevices(DeviceType.AITUM))[0];
+
+  await lib.sleep(250);
+
+  console.log('playing sound')
+  await aitumDevice.playSound('/Users/dussed/Downloads/DJ Fresh VS Jay Fay ft. Ms Dynamite - Dibby Dibby Sound [Official Video] [81Mw7Z9AOkw].m4a', 0.5);
+
+  await lib.sleep(7500);
+
+  console.log('stopping sound')
+  await aitumDevice.stopAllSounds();
 };
+
+/*********** DON'T EDIT BELOW ***********/
+export default { name, inputs, method } as ICustomCode;
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,23 @@
 import { AitumCCLib } from 'aitum-cc-lib';
-import { InputType } from 'aitum-cc-lib/src/enums/InputType';
 import * as dotenv from 'dotenv';
 import { resolve } from 'path';
+
 import DummyAction from './actions/DummyAction';
 
 dotenv.config({ path: resolve(__dirname, '..', 'settings.env') });
 
 const lib = AitumCCLib.get();
+
 (async () => {
   // Set up the environment
   lib.setEnv(process.env.AITUM_CC_ID as string, process.env.AITUM_CC_HOST as string, process.env.API_KEY as string);
 
   // Register actions
-  lib.registerAction('Dummy Action', {
-    testStringInput: {
-      type: InputType.STRING,
-      label: 'What is your name?',
-      required: true
-    },
-    testBooleanInput: {
-      type: InputType.BOOLEAN,
-      label: 'Are you a fun person?',
-      required: false
-    },
-    testIntInput: {
-      type: InputType.INT,
-      label: 'How old are you?',
-      required: true
-    },
-    testFloatInput: {
-      type: InputType.FLOAT,
-      label: 'Volume level',
-      required: false
-    }
-  }, DummyAction);
+  lib.registerAction(DummyAction);
 
 
   // Connect after a few seconds
-  setTimeout(async () => await lib.connect(), 5e3);
+  setTimeout(async () => await lib.connect(), 1e3);
 })();
 
 
